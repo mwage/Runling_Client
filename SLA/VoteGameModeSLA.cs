04fec88e591b8636bd9c93d0f6ecd378d80ca189
Michal Lukasiak <fisher@onet.eu>
2017-07-21 16:42:29 +0200

    Speed and boost work

--- SLA/VoteGameModeSLA.cs
+++ SLA/VoteGameModeSLA.cs
@@ -10,53 +10,43 @@ namespace SLA
 {
     public class VoteGameModeSLA : MonoBehaviour
     {
-        [SerializeField] private GameObject _countdownPrefab;
-        [SerializeField] private GameObject _mode;
-        [SerializeField] private GameObject _finishButton;
-        public Text _classicVoteText;
-        public Text _teamVoteText;
-        public Text _practiceVoteText;
+        [SerializeField] private NetworkManagerSLA _networkManagerSLA;
 
-        private PhotonView _photonView;
-        private bool[] _finishedVoting;
+        [SerializeField] private GameObject _countdownPrefab;
+        [SerializeField] private GameObject _finishButton;
+        [SerializeField] private Text _classicVoteText;
+        [SerializeField] private Text _teamVoteText;
+        [SerializeField] private Text _practiceVoteText;
+
+        public PhotonView PhotonView;
         private Gamemode?[] _votes;
-        private PhotonPlayer[] _playerList;
         private int _classicVotes;
         private int _teamVotes;
         private int _practiceVotes;
-        private Dictionary<Gamemode, int> _votesPerMode = new Dictionary<Gamemode, int>();
+        private Dictionary<Gamemode, int> _votesPerMode;
+        private bool _starting;
+
+        public VoteGameModeSLA(bool starting)
+        {
+            _starting = starting;
+        }
 
         private void Awake()
         {
-            _photonView = GetComponent<PhotonView>();
-            _finishedVoting = new bool[PhotonNetwork.room.PlayerCount];
+            PhotonView = GetComponent<PhotonView>();
             _votes = new Gamemode?[PhotonNetwork.room.PlayerCount];
-            _playerList = new PhotonPlayer[PhotonNetwork.room.PlayerCount];
-            _votesPerMode.Add(Gamemode.Classic, _classicVotes);
-            _votesPerMode.Add(Gamemode.Team, _teamVotes);
-            _votesPerMode.Add(Gamemode.Practice, _practiceVotes);
-        }
-
-        private void Start()
-        {
-            foreach (var player in PhotonNetwork.playerList)
+            _votesPerMode = new Dictionary<Gamemode, int>
             {
-                _playerList[player.ID - 1] = player;
-            }
-            foreach (var player in _playerList)
-            {
-                Debug.Log(player.NickName + ": " + player.ID);
-            }
-
-            if (PhotonNetwork.isMasterClient)
-            {
-                _photonView.RPC("StartVoting", PhotonTargets.All);
-            }
+                {Gamemode.Classic, _classicVotes},
+                {Gamemode.Team, _teamVotes},
+                {Gamemode.Practice, _practiceVotes}
+            };
         }
 
         [PunRPC]
         private void StartVoting()
         {
+            _networkManagerSLA.Voting = true;
             StartCoroutine(Voting());
         }
 
@@ -66,7 +56,7 @@ namespace SLA
             // Countdown
             for (var i = 0; i < countDownFrom; i++)
             {
-                var countdown = Instantiate(_countdownPrefab, GameObject.Find("Canvas").transform);
+                var countdown = Instantiate(_countdownPrefab, transform);
                 countdown.GetComponent<RectTransform>().anchoredPosition = new Vector2(-200, 150);
                 countdown.GetComponent<TextMeshProUGUI>().text = (countDownFrom - i).ToString();
                 yield return new WaitForSeconds(1);
@@ -78,17 +68,14 @@ namespace SLA
 
         private void Update()
         {
-            if (_finishedVoting.Any(finished => !finished))
+            if (_networkManagerSLA.PlayerState == null || _starting)
+                return;
+            if (_networkManagerSLA.PlayerState.Where(state => state != null).Any(state => !state.FinishedVoting))
                 return;
 
+            _starting = true;
             if (PhotonNetwork.isMasterClient)
-                StartGame();
-        }
-
-        private void StartGame()
-        {
-            _photonView.RPC("SetGameMode", PhotonTargets.All);
-            PhotonNetwork.LoadLevel(5);
+                PhotonView.RPC("StartGame", PhotonTargets.All);
         }
 
         #region Buttons
@@ -110,13 +97,13 @@ namespace SLA
 
         private void ChangeVote(Gamemode mode, int playerID)
         {
-            _photonView.RPC("SubmitVote", PhotonTargets.All, mode, playerID);
+            PhotonView.RPC("SubmitVote", PhotonTargets.All, mode, playerID);
         }
 
         [PunRPC]
         private void SubmitVote(Gamemode mode, int playerID)
         {
-            Debug.Log(_playerList[playerID - 1].NickName + " voted for " + mode);
+            Debug.Log(_networkManagerSLA.PlayerList[playerID - 1].NickName + " voted for " + mode);
             _votes[playerID - 1] = mode;
             UpdateVotes();
         }
@@ -155,20 +142,20 @@ namespace SLA
         public void Finish()
         {
             _finishButton.SetActive(false);
-            _photonView.RPC("FinishedVoting", PhotonTargets.All, PhotonNetwork.player.ID);
+            PhotonView.RPC("FinishedVoting", PhotonTargets.All, PhotonNetwork.player.ID);
         }
 
         [PunRPC]
         private void FinishedVoting(int playerID)
         {
-            Debug.Log(_playerList[playerID - 1].NickName + " is ready");
-            _finishedVoting[playerID - 1] = true;
+            Debug.Log(_networkManagerSLA.PlayerList[playerID - 1].NickName + " is ready");
+            _networkManagerSLA.PlayerState[playerID - 1].FinishedVoting = true;
         }
 
         [PunRPC]
-        private void SetGameMode()
+        private void StartGame()
         {
-            _mode.SetActive(false);
+            _starting = true;
             var random = new System.Random();
             var max = new[] {_classicVotes, _teamVotes, _practiceVotes}.Max();
             var mostVotes = _votesPerMode.Keys.Where(mode => _votesPerMode[mode] == max).ToList();
@@ -176,7 +163,8 @@ namespace SLA
             GameControl.GameState.SetGameMode = mostVotes[idx];
             GameControl.PlayerState.IsDead = true;
             GameControl.GameState.CurrentLevel = 1;
-            Debug.Log(GameControl.GameState.SetGameMode + " selected!");
+            transform.parent.gameObject.SetActive(false);
+            _networkManagerSLA.Game.SetActive(true);
         }
     }
 }
